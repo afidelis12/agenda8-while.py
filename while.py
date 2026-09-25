c38b8e62ebf5e56f3954e853125d9fcd99d372ca
Adriana Fidelis <267987263+afidelis12@users.noreply.github.com>
2026-09-25 19:17:16 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,7 @@
+valor = int(input("Digite 5 para continuar: "))
+
+while valor != 5:
+    print("Valor incorreto, tente novamente.")
+    valor = int(input("Digite 5 para continuar: "))
+
+print("Obrigado! Você digitou 5.")
